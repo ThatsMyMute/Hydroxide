8ae5ecc231b054cd972851971ae5abfc2bbc8b4f
ThatsMyMute <75107278+ThatsMyMute@users.noreply.github.com>
2024-11-19 10:55:19 -0500

    Update Constant.lua

--- objects/Constant.lua
+++ objects/Constant.lua
@@ -13,12 +13,12 @@ function Constant.new(closure, index, value)
 end
 
 function Constant.set(constant, value)
-    setconstant(constant.Closure, constant.Index, value)
+    debug.setconstant(constant.Closure, constant.Index, value)
     constant.Value = value
 end
 
 function Constant.update(constant)
-    constant.Value = getConstant(constant.Closure, constant.Index)
+    constant.Value = debug.getconstants(constant.Closure, constant.Index)
 end
 
 return Constant
